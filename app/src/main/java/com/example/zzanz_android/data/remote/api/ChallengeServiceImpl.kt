d2455cd669305be4469e7111e1603595b07cf942
yunakim2 <98_02@naver.com>
2023-08-02 01:22:48 +0900

    feat: 전체 예산 설정 및 카테고리 별 예산 설정 data layer 추가

--- app/src/main/java/com/example/zzanz_android/data/remote/api/ChallengeServiceImpl.kt
+++ app/src/main/java/com/example/zzanz_android/data/remote/api/ChallengeServiceImpl.kt
@@ -1,19 +1,46 @@
 package com.example.zzanz_android.data.remote.api
 
 import com.example.zzanz_android.data.remote.dto.ChallengeDto
+import com.example.zzanz_android.data.remote.dto.GoalAmountDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import javax.inject.Inject
 
 class ChallengeServiceImpl @Inject constructor(
     private val client: HttpClient
-): ChallengeService {
-    override suspend fun getChallengeParticipate(cursor: Int?, page: Int): List<ChallengeDto>{
-        return client.get("/challenge/participate"){
+) : ChallengeService {
+    override suspend fun getChallengeParticipate(cursor: Int?, page: Int): List<ChallengeDto> {
+        return client.get("/challenge/participate") {
             cursor?.let { parameter("cursor", it) }
             parameter("page", page)
         }.body()
+    }
+
+    override suspend fun postGoalAmount(goalAmountDto: GoalAmountDto): Any {
+        return client.post("/challenge/participate") {
+            contentType(ContentType.Application.Json)
+            setBody(goalAmountDto)
+        }
+    }
+
+    override suspend fun postCategoryGoalAmount(goalAmountListDto: List<GoalAmountDto>): Any {
+        return client.post("/challenge/plan/category") {
+            contentType(ContentType.Application.Json)
+            setBody(goalAmountListDto)
+        }
+    }
+
+    override suspend fun putGoalAmount(goalAmountDto: GoalAmountDto): Any {
+        return client.put("/challenge/participate/goalAmount") {
+            contentType(ContentType.Application.Json)
+            setBody(goalAmountDto)
+        }
     }
 }

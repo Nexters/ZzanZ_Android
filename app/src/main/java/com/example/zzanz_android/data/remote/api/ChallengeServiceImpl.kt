2fcac5cbd61485b2f6fe485f77ac02b96c9d5eec
yunakim2 <98_02@naver.com>
2023-08-02 20:09:01 +0900

    feat: GoalAmount 통신 ChallengeService 수정

--- app/src/main/java/com/example/zzanz_android/data/remote/api/ChallengeServiceImpl.kt
+++ app/src/main/java/com/example/zzanz_android/data/remote/api/ChallengeServiceImpl.kt
@@ -1,21 +1,26 @@
 package com.example.zzanz_android.data.remote.api
 
+import android.util.Log
+import com.example.zzanz_android.data.remote.dto.BaseResponseDto
 import com.example.zzanz_android.data.remote.dto.ChallengeDto
 import com.example.zzanz_android.data.remote.dto.GoalAmountDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.ClientRequestException
+import io.ktor.client.plugins.ServerResponseException
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
-import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
 import io.ktor.http.contentType
 import javax.inject.Inject
 
 class ChallengeServiceImpl @Inject constructor(
     private val client: HttpClient
 ) : ChallengeService {
+    private val TAG = this.javaClass.simpleName
     override suspend fun getChallengeParticipate(cursor: Int?, page: Int): List<ChallengeDto> {
         return client.get("/challenge/participate") {
             cursor?.let { parameter("cursor", it) }
@@ -23,24 +28,76 @@ class ChallengeServiceImpl @Inject constructor(
         }.body()
     }
 
-    override suspend fun postGoalAmount(goalAmountDto: GoalAmountDto): Any {
-        return client.post("/challenge/participate") {
-            contentType(ContentType.Application.Json)
-            setBody(goalAmountDto)
+    override suspend fun postGoalAmount(goalAmountDto: GoalAmountDto): Boolean {
+        return try {
+            client.post("/challenge/participate") {
+                contentType(ContentType.Application.Json)
+                setBody(goalAmountDto)
+            }.let {
+                it.status == HttpStatusCode.OK
+            }
+        } catch (e: ClientRequestException) {
+            // 4XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.body<BaseResponseDto>().message}")
+            return false
+        } catch (e: ServerResponseException) {
+            // 5XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.status.description}")
+            return false
+        } catch (e: Exception) {
+            Log.e(TAG, "Error : ${e.message}")
+            return false
+
         }
     }
 
-    override suspend fun postCategoryGoalAmount(goalAmountListDto: List<GoalAmountDto>): Any {
-        return client.post("/challenge/plan/category") {
-            contentType(ContentType.Application.Json)
-            setBody(goalAmountListDto)
+    override suspend fun postCategoryGoalAmount(goalAmountListDto: List<GoalAmountDto>): Boolean {
+        return try {
+            client.post("/challenge/plan/category") {
+                contentType(ContentType.Application.Json)
+                setBody(goalAmountListDto)
+            }.let {
+                it.status == HttpStatusCode.OK
+            }
+        } catch (e: ClientRequestException) {
+            // 4XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.body<BaseResponseDto>().message}")
+            return false
+        } catch (e: ServerResponseException) {
+            // 5XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.status.description}")
+            return false
+        } catch (e: Exception) {
+            Log.e(TAG, "Error : ${e.message}")
+            return false
         }
     }
 
-    override suspend fun putGoalAmount(goalAmountDto: GoalAmountDto): Any {
-        return client.put("/challenge/participate/goalAmount") {
-            contentType(ContentType.Application.Json)
-            setBody(goalAmountDto)
+    override suspend fun putGoalAmount(goalAmountDto: GoalAmountDto): Boolean {
+        return try {
+            client.post("/challenge/participate/goalAmount") {
+                contentType(ContentType.Application.Json)
+                setBody(goalAmountDto)
+            }.let {
+                it.status == HttpStatusCode.OK
+            }
+        } catch (e: ClientRequestException) {
+            // 4XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.body<BaseResponseDto>().message}")
+            return false
+        } catch (e: ServerResponseException) {
+            // 5XX response
+            // TODO - 에러시 오류 로직 처리
+            Log.e(TAG, "Error : ${e.response.status.description}")
+            return false
+        } catch (e: Exception) {
+            Log.e(TAG, "Error : ${e.message}")
+            return false
         }
     }
 }
---
+++ app/src/main/java/com/example/zzanz_android/data/remote/dto/BaseResponseDto.kt
@@ -0,0 +1,9 @@
+package com.example.zzanz_android.data.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class BaseResponseDto(
+    @SerialName("message") val message: String
+)

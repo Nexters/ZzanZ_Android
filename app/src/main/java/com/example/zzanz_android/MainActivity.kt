dbe559aa6bea40c2ec2c8fa1cafd9409a97b173b
yunakim2 <98_02@naver.com>
2023-08-03 16:39:13 +0900

    fix : windowCompat setDecorFitsSystemWindows 함수 제거

--- app/src/main/java/com/example/zzanz_android/MainActivity.kt
+++ app/src/main/java/com/example/zzanz_android/MainActivity.kt
@@ -28,7 +28,6 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             ZzanZ_AndroidTheme {
                 // A surface container using the 'background' color from the theme

3b4c6d0ad5ad1433c917e26c4d49e6af623fa1fa
yunakim2 <98_02@naver.com>
2023-08-03 18:18:07 +0900

    fix: keyboard 상태에따라 버튼 모양 변할 수 있도록 BottomGreenButton Compose 적용

--- app/src/main/java/com/example/zzanz_android/presentation/view/SettingScreen.kt
+++ app/src/main/java/com/example/zzanz_android/presentation/view/SettingScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -23,20 +25,26 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.zzanz_android.R
 import com.example.zzanz_android.common.NavRoutes
 import com.example.zzanz_android.common.SettingNavRoutes
+import com.example.zzanz_android.common.ui.theme.Dimen
 import com.example.zzanz_android.common.ui.theme.ZzanZColorPalette
 import com.example.zzanz_android.presentation.view.component.CategoryBottomSheet
 import com.example.zzanz_android.presentation.view.component.GreenRectButton
 import com.example.zzanz_android.presentation.view.component.GreenRoundButton
 import com.example.zzanz_android.common.ui.util.keyboardAsState
+import com.example.zzanz_android.presentation.view.component.BottomGreenButton
+import com.example.zzanz_android.presentation.view.component.GreenButton
 import com.example.zzanz_android.presentation.view.setting.AlarmSetting
 import com.example.zzanz_android.presentation.view.setting.BudgetByCategory
 import com.example.zzanz_android.presentation.view.setting.BudgetCategory
@@ -52,9 +60,8 @@ fun Setting(navController: NavHostController, route: String = SettingNavRoutes.B
     var isButtonEnabled by remember { mutableStateOf(true) }
     val coroutineScope = rememberCoroutineScope()
     val sheetState = rememberModalBottomSheetState()
+    val isKeyboardOpen by keyboardAsState()
 
-    // TODO - keyboardAsState 동작 안함
-    var isKeyboardOpen by keyboardAsState()
     when (route) {
         SettingNavRoutes.BudgetByCategory.route -> {
             nextRoute = SettingNavRoutes.AlarmSetting.route
@@ -121,66 +128,27 @@ fun Setting(navController: NavHostController, route: String = SettingNavRoutes.B
                 }
             }
             Spacer(modifier = Modifier.weight(1f))
-            SetBottomButton(
+            BottomGreenButton(
                 buttonText = buttonText,
-                navController = navController,
-                nextRoute = nextRoute,
+                onClick = {
+                    if (isButtonEnabled) {
+                        navController.navigate(nextRoute) {
+                            popUpTo(NavRoutes.Setting.route) {
+                                inclusive = true
+                            }
+                        }
+                    }
+                },
                 isButtonEnabled = isButtonEnabled,
                 isKeyboardOpen = isKeyboardOpen,
+                horizontalWidth = if (isKeyboardOpen) 0 else 24
             )
+
             if (sheetState.isVisible) {
                 CategoryBottomSheet(coroutineScope = coroutineScope, sheetState = sheetState)
             }
         }
     }
-}
-
-
-@Composable
-fun SetBottomButton(
-    buttonText: String,
-    navController: NavHostController,
-    nextRoute: String,
-    isButtonEnabled: Boolean,
-    isKeyboardOpen: Boolean
-) {
-    if (isKeyboardOpen) {
-        GreenRectButton(
-            modifier = Modifier
-                .height(56.dp)
-                .padding(horizontal = 24.dp),
-            text = buttonText,
-            onClick = {
-                if (isButtonEnabled) {
-                    navController.navigate(nextRoute) {
-                        popUpTo(NavRoutes.Setting.route) {
-                            inclusive = true
-                        }
-                    }
-                }
-            },
-            enabled = isButtonEnabled
-        )
-    } else {
-        GreenRoundButton(
-            modifier = Modifier
-                .height(56.dp)
-                .padding(horizontal = 24.dp),
-            text = buttonText,
-            onClick = {
-                if (isButtonEnabled) {
-                    navController.navigate(nextRoute) {
-                        popUpTo(NavRoutes.Setting.route) {
-                            inclusive = true
-                        }
-                    }
-                }
-            },
-            enabled = isButtonEnabled
-        )
-        Spacer(modifier = Modifier.height(24.dp))
-    }
-
 }
 
 @Composable

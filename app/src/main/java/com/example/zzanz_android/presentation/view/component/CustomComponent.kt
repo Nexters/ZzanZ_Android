a9017dc90059868a6bd5cf403b93c77215184439
gowoon-choi <gowoonvv@gmail.com>
2023-08-03 17:42:05 +0900

    remove unused import

--- app/src/main/java/com/example/zzanz_android/presentation/view/component/CustomComponent.kt
+++ app/src/main/java/com/example/zzanz_android/presentation/view/component/CustomComponent.kt
@@ -39,8 +39,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
